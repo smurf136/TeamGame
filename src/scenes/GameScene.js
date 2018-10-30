4c34a3632c1f98427997efb6bdf1f85daae4aa04
smurf136 <apiwat.it24@mail.kmuu.ac.th>
2018-10-30 17:39:36 +0700

    insert bg

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -7,12 +7,12 @@ class GameScene extends Phaser.Scene {
 
     preload() {
         this.load.image('sky','assets/sky.png');
-        this.load.image('star','assets/star.png')
+        this.load.image('bomb','assets/bomb.png')
     }
 
     create() {
-        this.add.image(0, 0, 'sky')
-        this.add.image(0, 0, 'star').setOrigin(0,0)
+        this.add.image(400, 300, 'sky')
+        this.add.image(0, 0, 'bomb').setOrigin(0,0)
     }
 
     update() {
